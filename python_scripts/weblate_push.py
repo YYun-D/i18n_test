56825ee0d99dd7a9ab109ce2d25e12d937c90be1
YYun-D <85883811+YYun-D@users.noreply.github.com>
2022-11-12 13:33:11 +0900

    Create weblate_push.py

---
+++ python_scripts/weblate_push.py
@@ -0,0 +1,27 @@
+import requests
+import json
+
+url = "http://40.76.85.234/api/translations/test/readme-example/ko/"
+
+payload={}
+headers = {}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+result = json.loads(response.text)
+print(result["translated_percent"])
+
+if result["translated_percent"]>=75:
+    
+    url = "http://40.76.85.234/api/translations/test/readme-example/ko/repository/"
+
+    payload = json.dumps({
+    "operation": "push"
+    })
+    headers = {
+    'Authorization': 'Bearer wlu_oY6KhWRDjIpLWPuoYZMD4IL3fU2lmHEvCuvw',
+    'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+    print(response.text)
