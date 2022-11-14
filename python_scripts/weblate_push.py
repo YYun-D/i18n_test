013e6d4518f239d47f8bd574572d3a51d7f06980
YYun-D <85883811+YYun-D@users.noreply.github.com>
2022-11-14 11:21:57 +0900

    Update weblate_push.py

--- python_scripts/weblate_push.py
+++ python_scripts/weblate_push.py
@@ -1,6 +1,8 @@
 import requests
 import json
+import os
 
+BEARER_TOKEN = os.environ.get("BEARER_TOKEN")
 url = "http://azuresdkweblate.eastus.cloudapp.azure.com/api/translations/test/readme-example/ko/"
 
 payload={}
@@ -19,7 +21,7 @@ if result["translated_percent"]>=3:
     "operation": "push"
     })
     headers = {
-    'Authorization': 'Bearer wlu_oY6KhWRDjIpLWPuoYZMD4IL3fU2lmHEvCuvw',
+    'Authorization': BEARER_TOKEN,
     'Content-Type': 'application/json'
     }
 
