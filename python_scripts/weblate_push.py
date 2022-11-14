2b9ac2ab6450e47f2031ff63ca6e63dee5686625
YYun-D <85883811+YYun-D@users.noreply.github.com>
2022-11-14 11:30:14 +0900

    Update weblate_push.py

--- python_scripts/weblate_push.py
+++ python_scripts/weblate_push.py
@@ -10,15 +10,12 @@ response = requests.request("GET", url)
 result = json.loads(response.text)
 print(result["translated_percent"])
 
-if result["translated_percent"]>=3:
+if result["translated_percent"] >= 3:
     
     url = "http://azuresdkweblate.eastus.cloudapp.azure.com/api/translations/test/readme-example/ko/repository/"
 
     payload = json.dumps({ "operation": "push" })
-    headers = {
-    'Authorization': BEARER_TOKEN,
-    'Content-Type': 'application/json'
-    }
+    headers = { 'Authorization': BEARER_TOKEN, 'Content-Type': 'application/json' }
 
     response = requests.request("POST", url, headers=headers, data=payload)
     print(response.text)
